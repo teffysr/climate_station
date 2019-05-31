ceab02eb663ae88f38b711ae96f473c5a1cb23ec
teffy.sr <teffy.sr@gmail.com>
2019-05-31 00:58:26 -0300

    tabla datos de estaciones - vista

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,8 +5,13 @@
             <tr>
                 <th scope="col">Nombre</th>
                 <th scope="col">#</th>
-                <th scope="col">#</th>
-                <th scope="col">#</th>
+                <th scope="col">Localidad</th>
+                <th scope="col">Provinica</th>
+                <th scope="col">Latitud</th>
+                <th scope="col">Longitud</th>
+                <th scope="col">Altura</th>
+                <th scope="col">Ubicaciòn</th>
+                <th scope="col">Tipo</th>
             </tr>            
         </thead>
         <tbody>
@@ -14,8 +19,13 @@
                 <tr>
                     <th scope="col">{{ $station->name }}</th>
                     <th scope="col"><a href="{{ route('in_construction', $station->id) }}" class=""> Ver histórico</a></th>
-                    <th scope="col">-</th>
-                    <th scope="col">-</th>
+                    <th scope="col">{{ $station->locality }}</th>
+                    <th scope="col">{{ $station->province }}</th>
+                    <th scope="col">{{ $station->latitude }}</th>
+                    <th scope="col">{{ $station->longitude }}</th>
+                    <th scope="col">{{ $station->height }}</th>
+                    <th scope="col">{{ $station->location }}</th>
+                    <th scope="col">{{ $station->type }}</th>
                 </tr>
             @endforeach
         </tbody>
