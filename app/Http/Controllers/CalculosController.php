e893754501c35b56c8c10ad309904cfc8867eb99
teffy.sr <teffy.sr@gmail.com>
2019-05-31 02:11:38 -0300

    vista calculos

--- app/Http/Controllers/CalculosController.php
+++ app/Http/Controllers/CalculosController.php
@@ -3,15 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Model\Station;
 
 class CalculosController extends Controller
 {
     public function index()
     {
+    	$stations = Station::all();
     	return response()->view('calculos',[
         	'title' => 'Calculos', 
         	'active_stations' => '',
             'active_percentage' => '',
-            'active_calculos' => 'active']);
+            'active_calculos' => 'active',
+        	'stations' => $stations]);
     }
 }
--- resources/views/calculos.blade.php
+++ resources/views/calculos.blade.php
@@ -8,10 +8,31 @@
 	<div class="form-group">
 		<label for="station">Seleccione Estación</label>
     	<select class="form-control" id="station">
-      		<option>1</option>
-      		<option>2</option>
+        @foreach($stations as $s)
+      		<option value="{{ $s->id }}">{{ $s->name }}</option>
+        @endforeach  
     	</select>
   	</div>
+    <div class="row">
+      <div class="col-md-6">
+        <label for="station">Factor 1</label>
+        <div class="input-group input-group-sm mb-3">
+          <div class="input-group-prepend">
+            <span class="input-group-text" id="inputGroup-sizing-sm">F1</span>
+          </div>
+          <input type="text" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm">
+        </div>
+      </div>
+      <div class="col-md-6">
+        <label for="station">Factor 2</label>
+        <div class="input-group input-group-sm mb-3">
+          <div class="input-group-prepend">
+            <span class="input-group-text" id="inputGroup-sizing-sm">F2</span>
+          </div>
+          <input type="text" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm">
+        </div>
+      </div>
+    </div>
 </form>
 
     <a href="{{ route('home') }}" class="btn btn-info"> << Volver</a>
